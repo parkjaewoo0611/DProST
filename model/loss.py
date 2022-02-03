844728dc8ca8d1f2c2b9af9049d174f1f56af446
Jaewoo <bjw0611@snu.ac.kr>
2022-02-03 14:06:12 +0900

    hparam issue, pts_transform speed up, ycbv TODO

--- model/loss.py
+++ model/loss.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn.functional as F
 from pytorch3d.transforms import so3_relative_angle
-from utils.util import RT_to_vxvyvz, dynamic_projective_stn, transform_pts
+from utils.util import RT_to_vxvyvz, transform_pts
 
 # TODO: fix geodesic_R + translation loss
 def geodesic_vxvyvz_loss(gt_RT, output, K_crop, **kwargs):
@@ -21,14 +21,14 @@ def grid_matching_loss(gt_RT, output, gt_grid, gt_dist, **kwargs):
     loss += F.l1_loss(pr_dist, gt_dist.detach())
     return loss
 
-def grid_matching_loss_without_distance(gt_RT, output, gt_grid, **kwargs):
+def grid_matching_wo_dist_loss(gt_RT, output, gt_grid, **kwargs):
     pr_grid = output['grid']
     loss = torch.sqrt(F.mse_loss(pr_grid, gt_grid.detach(), reduce=False).sum(-1) + 1e-9).mean()
     return loss
 
-def image_matching_loss(gt_RT, output, gt_grid, **kwargs):
-    pr_grid = output['grid']
-    loss = torch.sqrt(F.mse_loss(pr_grid, gt_grid.detach(), reduce=False).sum(-1) + 1e-9).mean()
+def image_matching_loss(gt_RT, output, gt_proj, **kwargs):
+    pr_proj = output['proj']
+    loss = F.l1_loss(pr_proj, gt_proj.detach())
     return loss
 
 def point_matching_loss(gt_RT, output, full_vertexes, **kwargs):
