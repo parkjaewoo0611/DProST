52c628386fcaf0cb0d180bcd59a1c2fd9962bce1
Park <bjw0611@snu.ac.kr>
2022-08-27 21:35:51 +0900

    cuda 11.0 -> 11.1 for compatibility

--- source_install.sh
+++ source_install.sh
@@ -2,7 +2,7 @@
 conda update -n base -c defaults conda -y
 conda create --name DProST python=3.7.0 -y
 source activate DProST
-conda install pytorch==1.7.0 torchvision==0.8.1 torchaudio==0.7.0 cudatoolkit=11.0 -c pytorch -y
+conda install pytorch==1.8.0 torchvision==0.9.0 torchaudio==0.8.0 cudatoolkit=11.1 -c pytorch -c conda-forge -y
 conda install -c fvcore -c iopath -c conda-forge fvcore iopath -y
 conda install jupyter -y
 conda install pytorch3d==0.5.0 -c pytorch3d -y
